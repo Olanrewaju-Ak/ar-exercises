0266681690020033dcf4d371f9d4931a02e76242
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-20 18:54:05 -0700

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum(:annual_revenue)
+
+puts "total revenue = #{total}"
+
+average_revenue = (Store.sum(:annual_revenue))/(Store.count)
+puts "average revenue = #{average_revenue}"
+
+profit_stores = Store.where("annual_revenue >=1000000").count
+puts "profit_stores = #{profit_stores}"
