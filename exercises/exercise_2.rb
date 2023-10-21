919c113297175246164e391d0c942678d5eb3d9c
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-20 18:17:06 -0700

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.second
+
+
+#updating store1 using two methods
+#method 1
+@store1.name = "Surrey"
+@store1.save
+
+#method 2
+@store1.update(annual_revenue: 16000000)
+
