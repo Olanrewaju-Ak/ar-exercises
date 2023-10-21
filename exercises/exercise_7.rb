d137577db38e22f5713ba8b77c5cb856ad60475f
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-21 09:07:46 -0700

    Add validations and complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "enter a store name"
+ user_input = $stdin.gets.chomp
+ 
+ @new_store = Store.create(name: user_input, annual_revenue: 12430, mens_apparel: false, womens_apparel: false)
+
+@new_store.save
+
+puts @new_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,27 @@
 class Store < ActiveRecord::Base
+	
  has_many :employees
+ validates :name, presence: true,length: {minimum: 3, message: "3 characters is the minimum allowed"}
+ validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+ #custom validator :validates that a store carries either men or women's apparel
+ validate :must_have_men_or_women_apparel
+
+	private
+
+	def must_have_men_or_women_apparel
+		if(mens_apparel == false && womens_apparel == false)
+			errors.add(:mens_apparel, ' must be present')
+			errors.add(:womens_apparel, 'must be present')
+		end
+	end
+
 end
+
+# class MyValidator < Activemodel::MyValidator
+#  def validate(record)
+# 	if (store.name.length < 3)
+# 		records.errors.add(:base, "This record is invalid")
+# 	end
+#  end
+# end
